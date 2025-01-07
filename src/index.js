bf472bd30b76abf28d7b8301c091452008e16211
Gaurang Verma <gaurangvr16@gmail.com>
2025-01-07 08:42:04 +0530

    Utils

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import connectDB from './db/index.js';
+//import app from './app.js';
 
 dotenv.config({
     path:  './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`App is listening at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed: ", err);
+})
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse{
+    constructor(statusCode, data, message= "Success"){
+        this.statusCode = statusCode;
+        this.data = data;
+        this.messge = message;
+        this.success = statusCode < 400;
+    }
+}
+
+export {ApiResponse}
